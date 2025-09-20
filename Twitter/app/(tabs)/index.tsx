2f253d8564e51c189da1b13160f701fadcee4d88
m1lawd <155329122+m1lawd@users.noreply.github.com>
2025-09-20 23:48:15 +0330

    "Tweet component created and img-name-tweet rendred!!"

--- Twitter/app/(tabs)/index.tsx
+++ Twitter/app/(tabs)/index.tsx
@@ -1,31 +1,22 @@
-import { StyleSheet , Image } from 'react-native';
-
-import { Text, View } from '@/components/Themed';
+import { StyleSheet, View } from 'react-native';
+import Tweet from '@/components/Tweet';
 import tweets from '@/assets/data/tweets';
 
-const tweet = tweets[1];
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image src={tweet.user.image} style={styles.userImage}/>
-      <Text>
-        {tweet.content}
-      </Text>
-    </View>
+    <>
+      <Tweet tweet={tweets[0]}/>
+      <Tweet tweet={tweets[1]}/>
+      <Tweet tweet={tweets[2]}/>
+    </>
+    
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
- 
-  userImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-  }
+
+
 });
+
