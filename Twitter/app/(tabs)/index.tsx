ea0d910ae8995e03f22c0424ef02f1421a50fcf3
m1lawd <155329122+m1lawd@users.noreply.github.com>
2025-09-20 23:11:50 +0330

    "First Tweet Rendered"

--- Twitter/app/(tabs)/index.tsx
+++ Twitter/app/(tabs)/index.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet , Image } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import tweets from '@/assets/data/tweets';
+
+const tweet = tweets[1];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image src={tweet.user.image} style={styles.userImage}/>
+      <Text>
+        {tweet.content}
+      </Text>
     </View>
   );
 }
@@ -19,13 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+ 
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  }
 });
