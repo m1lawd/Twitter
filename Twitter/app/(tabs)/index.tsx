c86e1de82caec50ab38459dacb6ab555dccb3c48
m1lawd <155329122+m1lawd@users.noreply.github.com>
2025-09-21 00:29:50 +0330

    "Wroking on FlatList"

--- Twitter/app/(tabs)/index.tsx
+++ Twitter/app/(tabs)/index.tsx
@@ -1,22 +1,24 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import Tweet from '@/components/Tweet';
 import tweets from '@/assets/data/tweets';
 
 
 export default function TabOneScreen() {
   return (
-    <>
-      <Tweet tweet={tweets[0]}/>
-      <Tweet tweet={tweets[1]}/>
-      <Tweet tweet={tweets[2]}/>
-    </>
+    <View style={styles.page}>
+      <FlatList data={tweets} renderItem={({item}) => <Tweet tweet={item}/>}/>
+      {/* <Tweet tweet={tweets[0]}/> */}
+    </View>
     
   );
 }
 
 
 const styles = StyleSheet.create({
-
+  page: {
+    flex: 1,
+    backgroundColor: 'white',
+  }
 
 });
 
