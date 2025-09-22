31cbeabeb9e72c71ae5ccc85dbea3c365fe2f336
m1lawd <155329122+m1lawd@users.noreply.github.com>
2025-09-22 16:10:56 +0330

    "IconButton component Done!"

--- Twitter/components/Tweet.tsx
+++ Twitter/components/Tweet.tsx
@@ -1,23 +1,8 @@
 import {View , Text, Image, StyleSheet} from 'react-native';
 import { TweetType } from '@/types';
 import { Entypo , EvilIcons} from '@expo/vector-icons';
+import IconButton from './IconButton';
 
-
-type IconButtonProps = {
-  icon: React.ComponentProps <typeof EvilIcons> ['name'];
-  text?: string | number;
-}
-
-
-const IconButton = ({icon , text}: IconButtonProps) => {
-  return(
-    <View style={{flexDirection: 'row' , alignItems: 'center'}}>
-      <EvilIcons name={icon} size={22} color='gray'/>
-      <Text style={{fontSize: 12 , color: 'gray'}}>{text}</Text>
-    </View>
-  )
-
-}
 
 type TweetProps = {
   tweet: TweetType;
