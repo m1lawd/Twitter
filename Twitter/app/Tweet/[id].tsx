c0ab0cb48cf0df25e73f59dc1e0be15c45133ee2
m1lawd <155329122+m1lawd@users.noreply.github.com>
2025-09-25 21:20:44 +0330

    "useNAvigation imported and fixed hehader title BUG"

--- Twitter/app/Tweet/[id].tsx
+++ Twitter/app/Tweet/[id].tsx
@@ -2,12 +2,23 @@ import { Text } from "react-native";
 import Tweet from "@/components/Tweet";
 import tweets from "@/assets/data/tweets";
 import { useSearchParams } from "expo-router/build/hooks";
+import { useLayoutEffect } from "react";
+import { useNavigation } from "expo-router";
 
 export default function TweetScreen() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
+  const navigation = useNavigation();
 
   const tweet = tweets.find((t) => t.id === id);
+
+  useLayoutEffect(() => {
+    if (tweet) {
+      navigation.setOptions({
+        title: tweet.user.name, // 👈 change to tweet.user.username if you prefer
+      });
+    }
+  }, [tweet]);
 
   if (!tweet) {
     return <Text>Tweet {id} Not Found! </Text>;
